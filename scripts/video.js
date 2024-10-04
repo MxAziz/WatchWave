dde81ce9c24ea58a0f5968bbecad2dfa79ab0621
MxAziz <hereabdulaziz@gmail.com>
2024-10-04 16:02:16 +0600

    change the logo and fix the verify option

--- scripts/video.js
+++ scripts/video.js
@@ -25,12 +25,14 @@ const displayVideos = (videos) =>{
         const card = document.createElement('div');
         card.classList = "card card-compact "
         card.innerHTML = `
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative">
             <img
             src=${video.thumbnail}
             class="h-full w-full object-cover"
             alt="Shoes" />
+            <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
         </figure>
+
         <div class="px-0 py-2 flex gap-2">
             <div>
                 <img class="size-10 rounded-full object-cover" src=${video.authors[0].profile_picture} >
@@ -39,11 +41,10 @@ const displayVideos = (videos) =>{
                 <h2 class="font-bold">${video.title}</h2>
                 <div class="flex items-center gap-2">
                     <p class="text-gray-500">${video.authors[0].profile_name}</p>
-                    <img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" alt="">
+                    ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png">` : '' }
                 </div>
                 <p></p>
             </div>
-
 
         </,div>
         `
